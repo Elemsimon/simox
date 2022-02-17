ba081a9f452bb15f4cbdac9d3b723bbe1fe012ca
Simon Elem <54186763+Elemsimon@users.noreply.github.com>
2022-02-17 18:00:56 +0100

    update

--- src/components/faq/data.js
+++ src/components/faq/data.js
@@ -2,7 +2,7 @@ export const questions = [
   {
     id: 1,
     title: "Can I learn ReactJS?",
-    answer: "Yes, you can learn ReactJS at ZinoTrustAcademy.com",
+    answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores quae nisi impedit earum? Molestias eum laborum distinctio iste dolore! Deserunt cum veritatis laboriosam nihil, beatae voluptate sint officia obcaecati quae.",
   },
   {
     id: 2,
